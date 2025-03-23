040ae855dfe657405fb3ac773adea31b65e9fdc7
Adrian Wolny <adrian.wolny@bayer.com>
2025-03-23 23:45:54 +0100

    update feature extractor

--- get_features_CTransPath.py
+++ get_features_CTransPath.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pandas as pd
 import torch, torchvision
 import torch.nn as nn
@@ -35,21 +37,58 @@ class roi_dataset(Dataset):
 
         return image
 
-img_csv=pd.read_csv(r'./test_list.csv')
-test_datat=roi_dataset(img_csv)
-database_loader = torch.utils.data.DataLoader(test_datat, batch_size=1, shuffle=False)
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--pretrained-weights",
+        type=str,
+        help="Pretrained model weights",
+        default="./ctranspath.pth"
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=str,
+        help="Output directory to write results and logs",
+        required=True,
+    )
+    parser.add_argument(
+        "--dataset-dir",
+        type=str,
+        help="Path to the dataset directory",
+        required=True,
+    )
+    parser.add_argument(
+        "--train-set-path",
+        type=str,
+        help="Path to the train set pth file",
+        required=True,
+    )
+    parser.add_argument(
+        "--test-set-path",
+        type=str,
+        help="Path to the test set pth file",
+        required=True,
+    )
+    return parser
 
-model = ctranspath()
-model.head = nn.Identity()
-td = torch.load(r'./ctranspath.pth')
-model.load_state_dict(td['model'], strict=True)
+def main(args):
+
+    model = ctranspath()
+    model.head = nn.Identity()
+    td = torch.load(args.pretrained_weights)
+    model.load_state_dict(td['model'], strict=True)
+    model.eval()
+    with torch.no_grad():
+        # run inference on random batch
+        x = torch.randn(1, 3, 224, 224).cuda()
+        features = model(x)
+        print(features.shape)
+        # for batch in database_loader:
+        #     features = model(batch)
+        #     features = features.cpu().numpy()
 
 
-model.eval()
-with torch.no_grad():
-    for batch in database_loader:
-        features = model(batch)
-        features = features.cpu().numpy()
-
-
-#
+if __name__ == '__main__':
+    args_parser = get_args_parser()
+    args = args_parser.parse_args()
+    main(args)
